644dc65ca40632046893aafba95b46ca0b72f5f9
akingyin1987 <akingyin@163.com>
2015-07-24 10:56:30 +0800

    TEST

--- sample-app/src/main/java/rx/android/samples/MainActivity.java
+++ sample-app/src/main/java/rx/android/samples/MainActivity.java
@@ -4,16 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
 
-import java.sql.SQLOutput;
 import java.util.concurrent.TimeUnit;
+
 import rx.Observable;
 import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.android.schedulers.HandlerScheduler;
 import rx.exceptions.OnErrorThrowable;
+import rx.functions.Action1;
 import rx.functions.Func0;
 
 import static android.os.Process.THREAD_PRIORITY_BACKGROUND;
@@ -29,10 +31,15 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
-        System.out.println("fffffffffffS");
+
         BackgroundThread backgroundThread = new BackgroundThread();
         backgroundThread.start();
         backgroundHandler = new Handler(backgroundThread.getLooper());
+        Handler   testHandle = new Handler(){
+
+        };
+        mObservable.subscribeOn(AndroidSchedulers.mainThread())
+                    .observeOn(AndroidSchedulers.mainThread()).subscribe(mSubscriber);
 
         findViewById(R.id.scheduler_example).setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
@@ -81,4 +88,44 @@ public class MainActivity extends Activity {
             super("SchedulerSample-BackgroundThread", THREAD_PRIORITY_BACKGROUND);
         }
     }
+
+
+    //事件源
+    Observable<String>   mObservable = Observable.create(new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(Subscriber<? super String> subscriber) {
+             subscriber.onNext("测试完了");
+             System.out.println("线程：" + Thread.currentThread().getId());
+             subscriber.onCompleted();
+        }
+    }) ;
+
+
+    Subscriber<String>  mSubscriber  =  new Subscriber<String>() {
+        @Override
+        public void onCompleted() {
+            System.out.println("onCompleted");
+        }
+
+        @Override
+        public void onError(Throwable e) {
+            System.out.println("eeeee");
+        }
+
+        @Override
+        public void onNext(String s) {
+            System.out.println("线程："+Thread.currentThread().getId());
+           System.out.println(s);
+        }
+    };
+
+
+    Action1<String>  onNext = new Action1<String>() {
+        @Override
+        public void call(String s) {
+            System.out.println(Thread.currentThread().getId());
+            System.out.println(s);
+        }
+    };
+
 }
